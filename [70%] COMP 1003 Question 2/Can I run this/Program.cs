1dcfcd4af395043c8af3fea23618ef2e7953bb65
Javier Alvarez Sanzberro <jasanzberro@gmail.com>
2025-04-29 17:23:16 +0100

    Added a run-able program
    - Added Program.cs
     - Allows for program to run (also catches errors :/)

---
+++ [70%] COMP 1003 Question 2/Can I run this/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Can_I_run_this
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (i)
+            Dictionary<string, int> employees = new Dictionary<string, int>();
+
+            // (ii)
+            employees.Add("Homer", 20000);
+            employees.Add("Monty", 500000);
+
+            // (iii)
+            employees.Remove("Homer");
+
+            // (iv)
+            if (employees.TryGetValue("Carl", out int Carl_pay))
+            {
+                employees["Carl"] = Carl_pay - 1000;
+            }
+            else
+            {
+                employees.Add("Carl", -1000);
+            }
+
+            // (v)
+            foreach (var key in employees.Keys)
+            {
+                employees[key] = (int)(employees[key] * 1.01);
+                Console.WriteLine(employees[key]);
+
+            }
+
+        }
+    }
+}
--- [70%] COMP 1003 Question 2/[70%] COMP 1003 Question 2/Program.cs
+++ [70%] COMP 1003 Question 2/[70%] COMP 1003 Question 2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // (i)
+        Map<string, int> employees = new Map<string, int>();
+
+        // (ii)
+        employees.Add("Homer", 20000);
+        employees.Add("Monty", 500000);
+
+        // (iii)
+        employees.Remove("Homer");
+
+        // (iv)
+        int Carl_pay = employees.Get("Carl");
+        employees.Add("Carl", Carl_pay - 1000);
+
+        employees["Carl"] = employees["Carl"] - 1000;
+
+        // (v)
+
     }
 }
