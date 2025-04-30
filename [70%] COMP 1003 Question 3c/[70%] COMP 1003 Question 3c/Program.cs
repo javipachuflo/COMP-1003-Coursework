140fd818f412489f75401b0d05ea4631855b4eb2
Javier Alvarez Sanzberro <jasanzberro@gmail.com>
2025-04-30 21:36:46 +0100

    Initial commit
    - This one starts with a runnable program :D

---
+++ [70%] COMP 1003 Question 3c/[70%] COMP 1003 Question 3c/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _70___COMP_1003_Question_3c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
